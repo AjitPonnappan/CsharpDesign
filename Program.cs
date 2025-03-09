d6681455b67c98170fc1916f394071136dbff4bb
AjitPonnappan <ajit.ponnappana@gmail.com>
2025-03-09 15:43:04 +0530

    PicoGk files and csharp setup

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿
+try
+{
+	PicoGK.Library.Go(0.5f, test.App.run);
+}
+
+catch (Exception e)
+{
+	// Apparently something went wrong, output here
+	Console.WriteLine(e);
+}
---
+++ compressor.cs
@@ -0,0 +1,22 @@
+namespace impeller
+{
+    public class inlet()
+    {
+
+    }
+
+    public class inducer()
+    {
+
+    }
+
+    public class exducer()
+    {
+
+    }
+
+    public class outlet()
+    {
+        
+    }
+}
